599ecdce4d5bd18975665cf080092e875ee50f4c
Markus Weinzettl <futuresystems1323@gmail.com>
2026-06-16 07:23:21 +0200

    tag_handler expand

--- logic/tag_handler.py
+++ logic/tag_handler.py
@@ -2,20 +2,40 @@ import random
 import subprocess
 from datetime import datetime
 
-
 def handle_tag(tag, intent, speak):
     if tag == "open_notepad":
         speak(random.choice(intent['responses']))
-        subprocess.Popen("editor.exe", shell=True)
+        subprocess.Popen("notepad.exe", shell=True)
 
     elif tag == "close_notepad":
         speak(random.choice(intent['responses']))
-        subprocess.Popen("taskkill /f /im editor.exe", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        # Korrigiert von editor.exe auf notepad.exe
+        subprocess.Popen("taskkill /f /im notepad.exe", shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
     elif tag == "get_time":
         now = datetime.now().strftime("%H:%M")
         antwort = random.choice(intent['responses'])
         speak(f"{antwort} {now} Uhr.")
+
+    elif tag == "open_google":
+        speak(random.choice(intent['responses']))
+        subprocess.Popen("cmd /c start https://www.google.com", shell=True)
+
+    elif tag == "open_windows_settings":
+        speak(random.choice(intent['responses']))
+        subprocess.Popen("cmd /c start ms-settings:", shell=True)
+
+    elif tag == "volume_up":
+        speak(random.choice(intent['responses']))
+        subprocess.Popen("powershell -Command \"(New-Object -ComObject WScript.Shell).SendKeys([char]175)\"", shell=True)
+
+    elif tag == "volume_down":
+        speak(random.choice(intent['responses']))
+        subprocess.Popen("powershell -Command \"(New-Object -ComObject WScript.Shell).SendKeys([char]174)\"", shell=True)
+
+    elif tag == "volume_mute":
+        speak(random.choice(intent['responses']))
+        subprocess.Popen("powershell -Command \"(New-Object -ComObject WScript.Shell).SendKeys([char]173)\"", shell=True)
 
     elif tag == "goodbye":
         speak(random.choice(intent['responses']))
